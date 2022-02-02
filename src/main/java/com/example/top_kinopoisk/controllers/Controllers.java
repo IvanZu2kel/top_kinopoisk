52a249f0e790405be7a5bb8aba53113049832952
Ivan Zu2kel <ivan_zukkel@mail.ru>
2022-02-02 22:46:10 +0300

    Thymeleaf

--- src/main/java/com/example/top_kinopoisk/controllers/Controllers.java
+++ src/main/java/com/example/top_kinopoisk/controllers/Controllers.java
@@ -5,17 +5,30 @@ import com.example.top_kinopoisk.service.FilmsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@RequestMapping("")
 public class Controllers {
     private final FilmsService filmsService;
 
-    @GetMapping
-    public ResponseEntity<DataFromXml> getFilmsByDate(@RequestParam(name = "year", defaultValue = "-1") int year) {
-        return new ResponseEntity<>(filmsService.getFilmsByDate(year), HttpStatus.OK);
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        String message = "";
+        model.addAttribute("message", message);
+
+        return "index.html";
+    }
+
+    @RequestMapping(value = { "/topFilms" }, method = RequestMethod.GET)
+    public String getFilmsByDate(@RequestParam(name = "year", defaultValue = "-1") int year,
+                                                      Model model) {
+        DataFromXml filmsByDate = filmsService.getFilmsByDate(year);
+        model.addAttribute("films", filmsByDate);
+        return "topFilms.html";
     }
 
 }
