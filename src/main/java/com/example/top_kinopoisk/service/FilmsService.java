64a47e61cea41573c57c6596a18a653d2d623779
Ivan Zu2kel <ivan_zukkel@mail.ru>
2022-02-02 21:46:56 +0300

    Работа с кэшем

--- src/main/java/com/example/top_kinopoisk/service/FilmsService.java
+++ src/main/java/com/example/top_kinopoisk/service/FilmsService.java
@@ -2,6 +2,7 @@ package com.example.top_kinopoisk.service;
 
 import com.example.top_kinopoisk.repository.FilmRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class FilmsService {
     private final FilmRepository filmRepository;
 
+    @Cacheable(cacheNames = "filmsCache", key = "#p0")
     public DataFromXml getFilmsByDate(int year) {
         if (year == -1) {
             return new DataFromXml().setFilms(filmRepository.findAll());
